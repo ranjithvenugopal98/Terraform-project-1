3bbb53d1b36ef75500c2c61ca40a2f598938e977
root <root@ip-172-31-93-107.ec2.internal>
2023-08-22 06:33:38 +0000

    adding files

---
+++ terraform-pipe/rds.tf
@@ -0,0 +1,24 @@
+resource "aws_db_subnet_group" "default" {
+  name = "main"
+  subnet_ids = [aws_subnet.database-subnet-1.id, aws_subnet.database-subnet-2.id]
+  tags = {
+    Name = "My DB subnet group"
+}
+}
+resource "aws_db_instance" "default" {
+  allocated_storage = 10
+  db_subnet_group_name = aws_db_subnet_group.default.id
+  engine = "mysql"
+  engine_version = "8.0.33"
+  instance_class = "db.r6g.16xlarge"
+  multi_az = true
+  db_name = "db_1"
+  username = "username"
+  password = "password"
+  skip_final_snapshot = true
+  vpc_security_group_ids = [aws_security_group.database-sg.id]
+  tags = {
+    db_identifier= "DB-1"
+
+}
+}
